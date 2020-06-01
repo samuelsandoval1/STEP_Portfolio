0ab1c6037ff8980701857fd08d317e970ef26e62
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-06-01 12:07:22 -0700

    Implement DataStore functionality for comments on website

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -7,16 +7,26 @@
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed onclear an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.sps.data.Comment;
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,19 +37,51 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    
-  private List<String> names;
- 
-  @Override
-  public void init() {
-    names = new ArrayList<String>(Arrays.asList("Sam", "Hadley", "Jerry"));
-  }
+  
+  /** Java object converter. */
+  private static final Gson gson = new Gson();
+
+  /** The max amount of comments that will be displayed. */
+  private int maxComments = 5;
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String name = names.get((int) (Math.random() * names.size()));
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello " + name + "!");
+    Query query = new Query("Comment").addSort("timeStamp", SortDirection.DESCENDING);
 
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    List<Entity> resultsList = results.asList(FetchOptions.Builder.withLimit(maxComments));
+
+    List<Comment> comments = new ArrayList<>();
+    for (Entity entity : resultsList) {
+      long id = entity.getKey().getId();
+      String author = (String) entity.getProperty("author");
+      String comment = (String) entity.getProperty("comment");
+      String timestamp = (String) entity.getProperty("timeStamp");
+
+      Comment commentObject = new Comment(id, author, comment, timestamp);
+      comments.add(commentObject);
+    }
+
+    response.setContentType("application/json;");
+    String json = gson.toJson(comments);
+    response.getWriter().println(json);
   }
+    
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+         String comment = request.getParameter("comment-text");
+        String author = "anonymous";
+        String timeStamp = "01/01/2020";
+
+        Entity commentEntity = new Entity("Comment");
+        commentEntity.setProperty("author", author);
+        commentEntity.setProperty("comment", comment);
+        commentEntity.setProperty("timeStamp", timeStamp);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+        response.sendRedirect("/index.html");
+    }
 }
