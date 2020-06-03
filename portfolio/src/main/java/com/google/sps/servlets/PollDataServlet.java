6d270bddc99d497138307ac9308d4a26a294c8d3
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-06-03 22:16:05 +0000

    Added comments from latest PR

--- portfolio/src/main/java/com/google/sps/servlets/PollDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PollDataServlet.java
@@ -13,28 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 public class PollDataServlet extends HttpServlet {
 
   private LinkedHashMap<Integer, Integer> bigfootSightings = new LinkedHashMap<>();
+  private String bigfootSightingsJson;
 
   @Override
   public void init() {
     Scanner scanner = new Scanner(getServletContext().getResourceAsStream(
         "/WEB-INF/bigfoot-sightings-by-year.csv"));
+
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(",");
 
-      Integer year = Integer.valueOf(cells[0]);
-      Integer sightings = Integer.valueOf(cells[1]);
-
-      bigfootSightings.put(year, sightings);
+      if(cells.length > 0) {
+        if(cells[0] != "" && cells[1] != ""){
+          Integer year = Integer.valueOf(cells[0]);
+          Integer sightings = Integer.valueOf(cells[1]);
+          bigfootSightings.put(year, sightings);
+        }
+      } 
     }
+    Gson gson = new Gson();
+    bigfootSightingsJson = gson.toJson(bigfootSightings);
     scanner.close();
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
-    Gson gson = new Gson();
-    String json = gson.toJson(bigfootSightings);
-    response.getWriter().println(json);
+    response.getWriter().println(bigfootSightingsJson);
   }
 }
